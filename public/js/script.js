499faad068273057ab997800096fced83096079b
johnhattori <johnmhattori@yahoo.com>
2013-07-16 09:28:14 -0700

    added assets, css and js folders. Modified index.html

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+ $("#b01").hover(function(){
+      $(this).attr("src", "assets/buttons/alexis/alexis_b.gif");
+    },
+    function(){
+      $(this).attr("src", "assets/buttons/alexis/alexis.png");
+  });
+ $("#b02").hover(function(){
+      $(this).attr("src", "assets/buttons/alice/alice_b.gif");
+    },
+    function(){
+      $(this).attr("src", "assets/buttons/alice/alice.png");
+  });
+ $("#b03").hover(function(){
+      $(this).attr("src", "assets/buttons/amanda/amanda_b.gif");
+    },
+    function(){
+      $(this).attr("src", "assets/buttons/amanda/amanda01.jpg");
+  });
+ $("#b04").hover(function(){
+      $(this).attr("src", "assets/buttons/andrew/andrew_b.gif");
+    },
+    function(){
+      $(this).attr("src", "assets/buttons/andrew/andrew.png");
+  });
+ $("#b05").hover(function(){
+      $(this).attr("src", "assets/buttons/chris_c/chris_c_b.gif");
+    },
+    function(){
+      $(this).attr("src", "assets/buttons/chris_c/chris_c.png");
+  });
+
+
+
+});
