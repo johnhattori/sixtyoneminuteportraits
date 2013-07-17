0f1023aa7929a13f03b80463d6ee130566545721
johnhattori <johnmhattori@yahoo.com>
2013-07-17 09:36:50 -0700

    using http links for png's

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,5 @@
 $(document).ready(function(){
 
- $("#b01").hover(function(){
-      $(this).attr("src", "assets/buttons/alexis/alexis_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/alexis/alexis.png");
-  });
- $("#b02").hover(function(){
-      $(this).attr("src", "assets/buttons/alice/alice_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/alice/alice.png");
-  });
- $("#b03").hover(function(){
-      $(this).attr("src", "assets/buttons/amanda/amanda_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/amanda/amanda01.jpg");
-  });
- $("#b04").hover(function(){
-      $(this).attr("src", "assets/buttons/andrew/andrew_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/andrew/andrew.png");
-  });
- $("#b05").hover(function(){
-      $(this).attr("src", "assets/buttons/chris_c/chris_c_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/chris_c/chris_c.png");
-  });
- $("#b06").hover(function(){
-      $(this).attr("src", "assets/buttons/robert/robert_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/robert/robert.png");
-  });
- $("#b07").hover(function(){
-      $(this).attr("src", "assets/buttons/sarah/sarah_b.gif");
-    },
-    function(){
-      $(this).attr("src", "assets/buttons/sarah/sarah.png");
-  });
 
 
 
