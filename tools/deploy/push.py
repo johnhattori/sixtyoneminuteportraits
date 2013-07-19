d5fbaf366fba59ef4f43c59b4f20c5f733c1d8e3
Luca Pellicoro <lucap@jaman.net>
2013-07-19 00:32:18 -0700

    Flatten and upload a directory to s3 bucket

--- tools/deploy/push.py
+++ tools/deploy/push.py
@@ -1,16 +1,46 @@
 import os
 import sys
 import optparse
+import logging
 
 import boto
 from boto.s3.key import Key
 
-def upload(awskey, awssecret, bucket, mode, path):
+logging.basicConfig(format='%(levelname)s: %(message)s',
+					level=logging.INFO)
+
+def upload_files(awskey, awssecret, bucket, path):
+	logging.info("Uploading %s to bucket %s" % (path, bucket))
+
+	logging.info("Listing files...")
+	files = get_files(path)
+	logging.info("Found %s files to upload" % len(files))
+
+	logging.info("Connecting to S3")
+	conn = boto.connect_s3(awskey, awssecret)
+	b = conn.get_bucket(bucket)
+	k = Key(b)
+	for f in files:
+		file_name = os.path.basename(f)
+		logging.info("Uploading %s" % file_name)
+		k.key = file_name
+		k.set_contents_from_filename(f)
+		k.set_acl('public-read')
+
+def update_manifest(conn, bucket):
 	pass
-	#conn = boto.connect_s3()
+
+def get_files(path):
+	to_upload = []
+	if os.path.isfile(path):
+		to_upload.append(path)
+	else:
+		for root, _ , files in os.walk(path):
+			for f in files:
+				to_upload.append(os.path.join(root,f))
+	return to_upload
 
 def main():
-
     optparser = optparse.OptionParser(prog='push.py', version='0.1',
         description='Push files on Amazon S3',
         usage='%prog -k [access key id] -s [secret access key] ' + \
@@ -21,19 +51,14 @@ def main():
         default=os.getenv('AWS_SECRET_ACCESS_KEY'))
     optparser.add_option('--bucket', '-b', dest='bucket')
     optparser.add_option('--path', '-p', dest='path')
-    optparser.add_option('--mode', '-m', dest='mode', choices=['assets', 'client'])
     
     options, arguments = optparser.parse_args()
 
-    if not options.mode:
-    	optparser.print_help()
-    	sys.exit("Error: mode must be 'assets' or 'client'")
     if options.bucket and options.awskey and options.awssecret and options.path:
     	if os.path.exists(options.path):
-    		upload(options.awskey,
+    		upload_files(options.awskey,
     			options.awssecret,
     			options.bucket,
-    			options.mode,
     			options.path)
     	else:
     		sys.exit("Error: path does not exist: %s" % options.path)
@@ -43,3 +68,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
