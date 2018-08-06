9f87be0b0b377659874dd5d01b8a92a421fbef34
Luca Pellicoro <luca@easypost.com>
2018-08-05 22:55:01 -0700

    clean up

--- public/script.js
+++ public/script.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-	$('.fancybox-media').fancybox({
-		openEffect  : 'none',
-		closeEffect : 'none',
-		padding: 0,
-		helpers : { media: {}},
-		afterShow: playback_started
-	});
+  $('.fancybox-media').fancybox({
+    openEffect  : 'none',
+    closeEffect : 'none',
+    padding: 0,
+    helpers : { media: {}},
+    afterShow: playback_started
+  });
 
-	function playback_started() {
-		var iframe = $('.fancybox-iframe')[0];
-		var player = $f(iframe);
-		player.addEvent('ready', function() {
-			player.addEvent('finish', function(id) {$.fancybox.close();});
-		});
-	}
+  function playback_started() {
+    var iframe = $('.fancybox-iframe')[0];
+    var player = $f(iframe);
+    player.addEvent('ready', function() {
+      player.addEvent('finish', function(id) {$.fancybox.close();});
+    });
+  }
 
-	$.getJSON("portraits.json", function( data ) {
-		$(".portraits").append("<ul></ul>");
-	  	$.each( data, function( key, val ) {
-	  		portrait = "<li>"
-	    	portrait += "<a href='" + val["video"] + "' class='fancybox-media'>"
-	    	portrait += "<img class='img_a' src='" + val["thumbnail"] + "' alt='" + val["name"] + "'>"
-	    	portrait += "<div>" + val["name"] + "</div>"
-	    	portrait += "</a></li>"
-	    $(".portraits ul").append(portrait);
-	  	});
-	});
-
+  $.getJSON("portraits.json", function( data ) {
+    $(".portraits").append("<ul></ul>");
+    $.each( data, function( key, val ) {
+      portrait = "<li>"
+      portrait += "<a href='" + val["video"] + "' class='fancybox-media'>"
+      portrait += "<img class='img_a' src='" + val["thumbnail"] + "' alt='" + val["name"] + "'>"
+      portrait += "<div>" + val["name"] + "</div>"
+      portrait += "</a></li>"
+    $(".portraits ul").append(portrait);
+    });
+  });
 });
